aa1a4afc70cf96deb77ab2f19689337266843388
Jonathan Stevenson <jonathan@greatfigure8.com>
2022-02-20 18:26:27 -0600

    added updated on, created on, and by's

--- src/services/data/UxBatchService.ts
+++ src/services/data/UxBatchService.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import { ServerConfig } from "../../ServerConfig";
 import { IApiResponse } from "../models/data/Interfaces/Local/IApiResponse";
 import { ICreateBatchRequest } from "../models/data/Interfaces/ORDS/IConvManBatch";
@@ -12,8 +13,9 @@ export class UxBatchService extends OracleRestServiceBase {
     cnv_batch,
     created_by,
   }: ICreateBatchRequest): Promise<IApiResponse<ICreateBatchRequest>> => {
+    const nowAsJson = DateTime.now().toJSON().toString();
     return await this.runPost<ICreateBatchRequest>({
-      body: { pod_url, cnv_batch, created_by },
+      body: { pod_url, cnv_batch, created_by, created_on: nowAsJson, updated_by: created_by, updated_on: nowAsJson },
       contentType: "application/json",
     });
   };
