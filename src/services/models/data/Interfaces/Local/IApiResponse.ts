71fd9d5d0837a72b8dee0260c995720b7c5a79d2
Jonathan Stevenson <jonathan@greatfigure8.com>
2022-02-21 08:40:35 -0600

    working commit

--- src/services/models/data/Interfaces/Local/IApiResponse.ts
+++
@@ -1,9 +0,0 @@
-import { IOracleItem } from "../OracleApi/IOracleItem";
-import { IOracleApiError } from "../../../errors/IOracleApiError";
-import { IOracleResponse } from "../OracleApi/IOracleResponse";
-
-export interface IApiResponse<T> {
-  singleOracleItem?: T;
-  oracleResponse?: IOracleResponse<T>;
-  error?: IOracleApiError;
-}
--- src/services/models/data/Interfaces/OracleApi/IOracleItem.ts
+++ src/services/models/data/Interfaces/OracleApi/IOracleItem.ts
@@ -1,10 +1,8 @@
-import { ICnvSp } from "../ORDS/AutoRest/ICnvSpreadsheet";
+import { ICnvSpreadsheet } from "../ORDS/AutoRest/ICnvSpreadsheet";
 import { IUxBatchRequest } from "../ORDS/AutoRest/IUxBatchRequest";
-import { IUxPod } from "../ORDS/IUxPod";
-import { IUxPodEmail } from "../ORDS/IUxPodEmail";
+import { IUxPod } from "../ORDS/AutoRest/IUxPod";
+import { IUxPodEmail } from "../ORDS/AutoRest/IUxPodEmail";
 import { ILink } from "./IOracleLink";
-
-export interface IModuleItem {}
 
 export interface IPPDResp {
   links?: ILink[];
@@ -12,12 +10,12 @@ export interface IPPDResp {
 
 export interface IUxPodPPDResp extends IUxPod, IPPDResp {}
 export interface IUxPodEmailPPDResp extends IUxPodEmail, IPPDResp {}
-export interface ICnvSpPPDResp extends ICnvSp, IPPDResp {}
+export interface ICnvSpPPDResp extends ICnvSpreadsheet, IPPDResp {}
 export interface IBatchPPDResp extends IUxBatchRequest, IPPDResp {}
 
 export interface IOracleApiResponse {}
 
-export interface IGetResponse<T extends IOracleModuleItem> {
+export interface IGetResponse<T> {
   items: Array<T>;
 }
 
