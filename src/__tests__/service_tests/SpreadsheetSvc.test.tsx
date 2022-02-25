98852eeb473d551075a8d78153d91d5d61cf4d54
Jonathan Stevenson <jonathan@greatfigure8.com>
2022-02-24 21:45:41 -0600

    Fix the row creator in the excel service
    To not include columns that have a value starting with MERGE

--- src/__tests__/service_tests/SpreadsheetSvc.test.tsx
+++ src/__tests__/service_tests/SpreadsheetSvc.test.tsx
@@ -1,3 +1,7 @@
+import { readFileSync } from "fs";
+import { DateTime } from "luxon";
+
+import { IConvManFile } from "../../components/forms/interfaces/IConvManFileInputState";
 import { SpreadsheetsSvc } from "../../services/SpreadsheetSvc";
 
 describe("GET tests", () => {
@@ -18,5 +22,51 @@ describe("GET tests", () => {
       expect(resp.entities.length).toBeGreaterThan(0);
       done();
     });
+  }, 60000);
+});
+
+describe("PUT, POST, DELETE tests", ()=> {
+
+let svc: SpreadsheetsSvc;
+
+  beforeAll((): void => {
+    svc = new SpreadsheetsSvc();
+  });
+
+  beforeEach((): void => {
+    jest.useRealTimers();
+    jest.setTimeout(60000);
+  });
+
+
+  it("should read a file", (done: jest.DoneCallback) => {
+    const dataArray = readFileSync("./public/templates/DC001.xlsx");
+    expect(dataArray).not.toBeUndefined();
+    expect(dataArray.length).toBeGreaterThan(100);
+    done();
+  });
+
+  it("should create a batch request and a CSV bulkload", (done: jest.DoneCallback) => {
+    const file: IConvManFile = {
+      fileExt: ".xlsx",
+      fileName: "test.xlsx",
+      lastModified: DateTime.now().toJSON(),
+      data: readFileSync("./public/templates/DC001.xlsx"),
+    };
+
+    svc
+      .createBatch({
+        batchName: DateTime.now().toMillis().toString(),
+        podUrl: "https://www.oracle.com/test",
+        createdBy: "jsteve81@gmail.com",
+        file: file,
+        sheet: "Grades",
+      })
+      .then((resp) => {
+        expect(resp).not.toBeUndefined();
+        expect(resp.batchCreateResponse.entities.length).toEqual(1);
+        expect(resp.spreadsheetCreateResponse).not.toBeUndefined();
+        done();
+      });
   }, 60000);
 });
--- src/services/SpreadsheetSvc.ts
+++ src/services/SpreadsheetSvc.ts
@@ -1,8 +1,14 @@
+import Papa from "papaparse";
+
+import { IConvManFile } from "../components/forms/interfaces/IConvManFileInputState";
 import { IUniqueWorksheet } from "../models/entities/api/IUniqueWorksheet";
 import { ICnvSpreadsheet } from "../models/entities/base/ICnvSpreadsheet";
 import { IApiResponse } from "../models/responses/IApiResponse";
+import { ICreateBatchResponse } from "../models/responses/ICreateBatchResponse";
 import { ServerConfig } from "../ServerConfig";
 import { OracleRestServiceBase } from "./base/OracleRestServiceBase";
+import { BatchRequestSvc } from "./BatchRequestSvc";
+import ExcelSvc from "./ExcelSvc";
 
 export class SpreadsheetsSvc extends OracleRestServiceBase {
   constructor() {
@@ -37,37 +43,50 @@ export class SpreadsheetsSvc extends OracleRestServiceBase {
     }
     return response;
   };
+
+  createBatch = async ({
+    file,
+    podUrl,
+    batchName,
+    createdBy,
+    sheet,
+  }: {
+    file: IConvManFile;
+    podUrl: string;
+    batchName: string;
+    createdBy: string;
+    sheet: string;
+  }): Promise<ICreateBatchResponse> => {
+    const excel = new ExcelSvc();
+    const spRows = excel.sheetToCsv({ workbook: file, sheetToRead: sheet, batchName: batchName });
+    const csv = Papa.unparse(spRows);
+    const batchSvc = new BatchRequestSvc();
+
+    const batchReqResp = await batchSvc.createBatchRequest({
+      pod_url: podUrl,
+      cnv_batch: batchName,
+      created_by: createdBy,
+      ux_batch_request_id: null,
+      created_on: null,
+      updated_by: null,
+      updated_on: null,
+    });
+
+    const response: ICreateBatchResponse = {
+      batchCreateResponse: { entities: [], error: { message: "", name: "" } },
+      spreadsheetCreateResponse: { entities: [], error: { message: "", name: "" } },
+    };
+    try {
+      if (batchReqResp) response.batchCreateResponse = batchReqResp;
+      const createSpResp = await this.runPost<any, any>({
+        action: ServerConfig.ords.customActions.posts.batchload,
+        body: csv,
+        contentType: "text/csv",
+      });
+      response.spreadsheetCreateResponse.entities.push({ ...createSpResp });
+    } catch (e) {
+      response.spreadsheetCreateResponse = this.handleError({ e, code: "POST", reqType: "ORDS_API_EXCEPTION" });
+    }
+    return response;
+  };
 }
-
-// async saveFile({
-//   file,
-//   podUrl,
-//   batchName,
-//   createdBy,
-//   sheet,
-// }: {
-//   file: IConvManFile;
-//   podUrl: string;
-//   batchName: string;
-//   createdBy: string;
-//   sheet: string;
-// }): Promise<IApiResponse<any>> {
-//   const excel = new ExcelService();
-//   const spRows = excel.sheetToCsv({ workbook: file, sheetToRead: sheet, batchName: batchName });
-//   const csv = Papa.unparse(spRows);
-
-//   const uxBatchSvc = new UxBatchService();
-//   const batchReqResp = await uxBatchSvc.createBatchRequest({
-//     pod_url: podUrl,
-//     cnv_batch: batchName,
-//     created_by: createdBy,
-//   });
-
-//   return batchReqResp && batchReqResp.error
-//     ? batchReqResp
-//     : await this.runPost<any>({
-//         action: ServerConfig.ords.customActions.posts.batchload,
-//         body: csv,
-//         contentType: "text/csv",
-//       });
-// }
